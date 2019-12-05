33281344df1a946e97c50d0a8eb666f86952786d
Patrick O'Neill <paddyjoneill@hotmail.com>
2019-12-05 15:01:22 +0000

    basic structure

--- models/casting.rb
+++ models/casting.rb
@@ -1,3 +1,13 @@
 class Casting
 
+  attr_reader :id
+  attr_accessor :movie_id, :star_id, :fee
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @movie_id = options['movie_id'].to_i
+    @star_id = options['star_id'].to_i
+    @fee = options['fee'].to_i
+  end
+
 end
--- models/star.rb
+++ models/star.rb
@@ -1,3 +1,12 @@
 class Star
 
+  attr_reader :id
+  attr_accessor :first_name, :last_name
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+  end
+
 end
